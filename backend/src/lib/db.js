fdd80c7eb022da63c11b036a44d0b51d2c3af58e
Ankur96-design <ankurdamke5@gmail.com>
2025-11-03 04:01:49 +0530

    signup section code refactoring

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,6 +2,9 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
     try {
+        const { MONGO__URL} = process.env;
+        if (!MONGO__URL)  throw new Error("MONGO_UL is not set");
+
         const conn = await mongoose.connect(process.env.MONGO_URL)
         await mongoose.connect(process.env.MONGO_URL)
         console.log("MONGODB CONNECTED:", conn.connection.host);
