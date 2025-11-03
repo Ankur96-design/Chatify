3a616a2e54a0cdce7415d770e6c844e87a3c4c7e
Ankur96-design <ankurdamke5@gmail.com>
2025-11-03 14:23:12 +0530

    update profile endpoint added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
+import cookieParser from "cookie-parser"
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
+
 import { connectDB } from './lib/db.js';
 import { ENV } from './lib/env.js';
 
@@ -17,6 +19,7 @@ console.log(process.env.PORT);
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json()); // req.body
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
